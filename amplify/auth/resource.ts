5cd94afabd35e8ed556e1edfc4ab569364cc997d
saviorab1 <popww147@gmail.com>
2025-06-19 15:50:58 +0700

    Authenticator

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -6,6 +6,11 @@ import { defineAuth } from '@aws-amplify/backend';
  */
 export const auth = defineAuth({
   loginWith: {
-    email: true,
+    email: {
+      verificationEmailStyle:"CODE",
+      verificationEmailSubject:"This is an account registration test mail!",
+      verificationEmailBody:(createCode) =>
+        `Use this code to confirm your account: ${createCode()}`,
+    },
   },
 });
--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,11 +1,8 @@
-import { defineBackend } from '@aws-amplify/backend';
-import { auth } from './auth/resource';
-import { data } from './data/resource';
+import { defineBackend } from "@aws-amplify/backend";
+import { data } from "./data/resource";
+import { auth } from "./auth/resource";
 
-/**
- * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
- */
-defineBackend({
+const backend = defineBackend({
   auth,
   data,
 });
